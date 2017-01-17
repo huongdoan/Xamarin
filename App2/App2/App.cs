54cf4fbe6f4e338b6873687e6bece4c6f660f684
Huong <huong.doan@precisbeusiness.com.au>
2017-01-17 11:22:43 +0700

    Add project files.

---
+++ App2/App2/App.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App2
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            //var content = new ContentPage
+            //{
+            //    Title = "App2",
+            //    Content = new StackLayout
+            //    {
+            //        VerticalOptions = LayoutOptions.Center,
+            //        Children = {
+            //            new Label {
+            //                HorizontalTextAlignment = TextAlignment.Center,
+            //                Text = "Welcome to Xamarin Forms!"
+            //            }
+            //        }
+            //    }
+            //};
+
+            var a = new CustomScanPage();
+            MainPage = a;
+            Device.StartTimer(new TimeSpan(0, 0, 0, 3, 0), () =>
+            {
+               if(a.zxing.IsScanning)
+                {
+                    a.zxing.AutoFocus();
+                }
+                return true;
+            });
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ App2/App2/Page_BarcodeScanner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public class Page_BarcodeScanner : ContentPage
+    {
+        public Page_BarcodeScanner()
+        {
+            Button btnScan = new Button
+            {
+                Text = "Scan Barcode",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            btnScan.Clicked += async (a, b) =>
+            {
+                Launch_BarcodeSearch();
+            };
+
+            this.Content = new StackLayout
+            {
+                Children =
+            {
+                btnScan,
+            },
+            };
+        }
+
+       async  void Launch_BarcodeSearch()
+        {
+           
+            var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+            var result =  await scanner.Scan();
+            if (result != null)
+                await this.DisplayAlert("Scanned Barcode: ",result.Text,"OK");
+        }
+    }
+}
